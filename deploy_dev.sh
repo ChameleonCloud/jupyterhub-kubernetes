fbd4f38ed4b12a951ccd92059f77f541aa8262b7
Mark Powers <markpowers@uchicago.edu>
2025-05-08 13:07:14 -0500

    Update config for shared cluster

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,21 +3,19 @@
 set -e
 set -x
 
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+#export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
 #gcloud auth login
 
-gcloud container clusters get-credentials jupyter-dev --zone us-central1
+#gcloud container clusters get-credentials jupyter-dev --zone us-central1
 
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
 helm upgrade --cleanup-on-fail \
 	--install jupyter-dev jupyterhub/jupyterhub \
-	--namespace jupyter-dev \
-	--create-namespace \
+	--namespace chameleon-jupyter \
 	--version=3.2.0 \
 	--values ./config/jupyterhub_config.yaml \
 	--values ./config/jupyterhub_secrets.yaml \
 	--values ./config/jupyterhub_config_dev.yaml
-
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -3,21 +3,19 @@
 set -e
 set -x
 
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+#export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
 #gcloud auth login
 
-gcloud container clusters get-credentials jupyterhub --zone us-central1-c
+#gcloud container clusters get-credentials jupyter-dev --zone us-central1
 
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
 helm upgrade --cleanup-on-fail \
-	--install jupyter jupyterhub/jupyterhub \
-	--namespace jupyter \
-	--create-namespace \
+	--install jupyter-prod jupyterhub/jupyterhub \
+	--namespace chameleon-jupyter \
 	--version=3.2.0 \
 	--values ./config/jupyterhub_config.yaml \
 	--values ./config/jupyterhub_secrets.yaml \
 	--values ./config/jupyterhub_config_prod.yaml
-
