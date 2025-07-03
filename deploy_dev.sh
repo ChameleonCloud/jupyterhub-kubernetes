5c562c3c38e24f7b2547ba8e79a5a51d0a16c97b
Mark Powers <markpowers@uchicago.edu>
2025-07-03 10:06:00 -0500

    Update to chart version 4.0

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,19 +3,13 @@
 set -e
 set -x
 
-#export USE_GKE_GCLOUD_AUTH_PLUGIN=True
-
-#gcloud auth login
-
-#gcloud container clusters get-credentials jupyter-dev --zone us-central1
-
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
 helm upgrade --cleanup-on-fail \
 	--install jupyter-dev jupyterhub/jupyterhub \
 	--namespace chameleon-jupyter \
-	--version=3.2.0 \
+	--version=4.0.0 \
 	--values ./config/jupyterhub_config.yaml \
 	--values ./config/jupyterhub_secrets.yaml \
 	--values ./config/jupyterhub_config_dev.yaml
--- deploy_local.sh
+++ deploy_local.sh
@@ -10,8 +10,10 @@ helm upgrade --cleanup-on-fail \
 	--install jupyter jupyterhub/jupyterhub \
 	--namespace jupyter \
 	--create-namespace \
-	--version=3.2.0 \
+	--version=4.0.0 \
 	--values ./config/jupyterhub_config.yaml \
 	--values ./config/jupyterhub_secrets.yaml \
 	--values ./config/jupyterhub_config_local_dev.yaml
 
+# sleep 10
+# kubectl -n jupyter exec -it $(kubectl get pods -n jupyter | grep hub | cut -d ' ' -f 1) -- bash -c 'pip install /jupyterhub_chameleon'
